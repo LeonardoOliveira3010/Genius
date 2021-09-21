3f26a49ceebd70ec679460737b6e4d01e1462608
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-09-20 21:22:56 -0300

    Criando a lógica do projeto Genius

---
+++ index.js
@@ -0,0 +1,18 @@
+let order = []
+let clickedOrder = []
+let score = 0
+
+const blue = document.querySelector('.blue')
+const yellow = document.querySelector('.yellow')
+const red = document.querySelector('.red')
+const green = document.querySelector('.green')
+
+let shuflerOrder = () =>{
+    let colorOrder = Math.floor(Math.random() * 4)
+    order: [order.length] = colorOrder;
+    clickedOrder = []
+
+    for(let i in order){
+        let elementColor = 
+    }
+}
